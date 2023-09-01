2170fb92f21dcc3eb5792c4d63389e27a043c7a4
Tharuka_00 <tharusenevi2000@gmail.com>
2023-09-01 10:57:02 +0530

    storing processed image

--- src/TableExtractor.py
+++ src/TableExtractor.py
@@ -1,4 +1,4 @@
-
+from PIL import Image
 
 
 class TableExtractor:
@@ -7,4 +7,18 @@ class TableExtractor:
         self.image_path = image_path
         
 
-   
+    # Read image - PIL Image
+    def read_image(self):
+        self.image = Image.open(self.image_path)
+
+    # Store processed image - PIL Image
+    def store_process_image(self, output_path, image):
+        image.save(output_path)
+
+    
+
+    def execute(self):
+        self.read_image()
+        self.store_process_image(
+            "./uploads/0_original.jpg", self.image)
+        
