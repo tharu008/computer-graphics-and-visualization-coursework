2c659949e102ad662d3fb17d44830f4d9d36d7f2
LAPTOP-F1KP92U9\ASUS <bimsara.jayatilake@gmail.com>
2023-09-14 22:17:26 +0530

    dialation and erosion of horizontal  and vertical lines in the perspective corrected image

--- src/TableLinesDetector.py
+++ src/TableLinesDetector.py
@@ -1,5 +1,6 @@
 import cv2
 from PIL import Image, ImageOps
+from skimage.morphology import rectangle, binary_erosion, binary_dilation
 import numpy as np
 import gaussian_filter as gf
 import gaussian_thresholding as gt
@@ -37,6 +38,52 @@ class TableLinesDetector:
     def invert_image(self):
         self.inverted_image = ImageOps.invert(self.thresholded_image)
  
+    # Extracting vertical lines - skimage
+    # erosion vertical lines
+    def v_erosion_image(self, iterations):
+        image_array = np.array(self.inverted_image)
+        vertical_kernel = rectangle(10, 1)
+        eroded_image_array = image_array.copy()  # Make a copy to preserve original
+        for _ in range(iterations):
+            eroded_image_array = binary_erosion(
+                eroded_image_array, vertical_kernel)
+        eroded_image_array = binary_erosion(
+            eroded_image_array, rectangle(6, 6))
+        self.v_eroded_image = Image.fromarray(eroded_image_array)
+
+    # dilation vertical lines - skimage
+    def v_dilation_image(self, iterations):
+        image_array = np.array(self.v_eroded_image)
+        vertical_kernel = rectangle(25, 1)
+        dilated_image_array = image_array.copy()  # Make a copy to preserve original
+        for _ in range(iterations):
+            dilated_image_array = binary_dilation(
+                dilated_image_array, vertical_kernel)
+        self.v_dilated_image = Image.fromarray(dilated_image_array)
+
+    # extracting horizontal lines
+    # erosion horizontal lines - skimage
+    def h_erosion_image(self, iterations):
+        image_array = np.array(self.inverted_image)
+        horizontal_kernel = rectangle(1, 10)
+        eroded_image_array = image_array.copy()  # Make a copy to preserve original
+        for _ in range(iterations):
+            eroded_image_array = binary_erosion(
+                eroded_image_array, horizontal_kernel)
+        eroded_image_array = binary_erosion(
+                eroded_image_array, rectangle(6, 6))
+        self.h_eroded_image = Image.fromarray(eroded_image_array)
+
+    # dilation horizontal lines - skimage
+    def h_dilation_image(self, iterations):
+        image_array = np.array(self.h_eroded_image)
+        horizontal_kernel = rectangle(1, 17)
+        dilated_image_array = image_array.copy()  # Make a copy to preserve original
+        for _ in range(iterations):
+            dilated_image_array = binary_dilation(
+                dilated_image_array, horizontal_kernel)
+        self.h_dilated_image = Image.fromarray(dilated_image_array)
+
  
 
     def execute(self):
@@ -53,3 +100,15 @@ class TableLinesDetector:
         self.invert_image()
         self.store_process_image(
             "./uploads/TableLinesDetector/19_inverteded.jpg", self.inverted_image)
+        self.v_erosion_image(iterations=12)
+        self.store_process_image(
+            "./uploads/TableLinesDetector/20_vertical_eroded.jpg", self.v_eroded_image)
+        self.v_dilation_image(iterations=10)
+        self.store_process_image(
+            "./uploads/TableLinesDetector/21_vertical_dilated.jpg", self.v_dilated_image)
+        self.h_erosion_image(iterations=12)
+        self.store_process_image(
+            "./uploads/TableLinesDetector/22_horizontal_eroded.jpg", self.h_eroded_image)
+        self.h_dilation_image(iterations=10)
+        self.store_process_image(
+            "./uploads/TableLinesDetector/23_horizontal_dilated.jpg", self.h_dilated_image)
