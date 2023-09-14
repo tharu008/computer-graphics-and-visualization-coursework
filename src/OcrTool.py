c74ee78cd34fb143c873020183fa8d45ab2ad1cf
Tharuka_00 <tharusenevi2000@gmail.com>
2023-09-14 22:39:39 +0530

    dilating the resulting image from the TableLineDetector class

--- src/OcrTool.py
+++ src/OcrTool.py
@@ -13,5 +13,21 @@ class OcrTool:
     def store_process_image(self, output_path, image):
         image.save(output_path)
 
-
-    #def execute(self):
+    def dilate_image(self):
+        kernel_to_remove_gaps_between_words = np.array([
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1]
+        ])
+        img_array = np.array(self.binarized_img)
+        self.dilated_image = cv2.dilate(
+            img_array, kernel_to_remove_gaps_between_words, iterations=1)
+        simple_kernel = np.ones((3, 3), np.uint8)
+        self.dilated_image = cv2.dilate(
+            self.dilated_image, simple_kernel, iterations=1)
+        self.dilated_image = Image.fromarray(self.dilated_image)
+    
+    def execute(self):
+        self.dilate_image()
+        self.store_process_image(
+           './uploads/OcrTool/28_dilated_image.jpg', self.dilated_image)
+        
