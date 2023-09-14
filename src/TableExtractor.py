c53d03b1972db7d2fc08f48863eadc0fc8f6a319
ArundiThamalsha <arundiherath98@gmail.com>
2023-09-14 21:00:38 +0530

    Dilation and Erosion of verticle and horizontal lines

--- src/TableExtractor.py
+++ src/TableExtractor.py
@@ -47,6 +47,37 @@ class TableExtractor:
         # Convert the eroded image array back to PIL Image
         self.v_eroded_image = Image.fromarray(eroded_image_array)
 
+        # dilation vertical lines - skimage
+    def v_dilation_image(self, iterations=5):
+        image_array = np.array(self.v_eroded_image)
+        vertical_kernel = rectangle(5, 1)
+        dilated_image_array = image_array.copy()  # Make a copy to preserve original
+        for _ in range(iterations):
+            dilated_image_array = binary_dilation(
+                dilated_image_array, vertical_kernel)
+        self.v_dilated_image = Image.fromarray(dilated_image_array)
+
+    # extracting horizontal lines
+    # erosion horizontal lines - skimage
+    def h_erosion_image(self, iterations=5):
+        image_array = np.array(self.inverted_image)
+        horizontal_kernel = rectangle(1, 5)
+        eroded_image_array = image_array.copy()  # Make a copy to preserve original
+        for _ in range(iterations):
+            eroded_image_array = binary_erosion(
+                eroded_image_array, horizontal_kernel)
+        self.h_eroded_image = Image.fromarray(eroded_image_array)
+
+    # dilation horizontal lines - skimage
+    def h_dilation_image(self, iterations=5):
+        image_array = np.array(self.h_eroded_image)
+        horizontal_kernel = rectangle(1, 5)
+        dilated_image_array = image_array.copy()  # Make a copy to preserve original
+        for _ in range(iterations):
+            dilated_image_array = binary_dilation(
+                dilated_image_array, horizontal_kernel)
+        self.h_dilated_image = Image.fromarray(dilated_image_array)
+
 
 
     def execute(self):
@@ -66,4 +97,14 @@ class TableExtractor:
         self.v_erosion_image(iterations=5)
         self.store_process_image(
             "./uploads/TableExtractor/4_vertical_eroded.jpg", self.v_eroded_image)
+        self.v_dilation_image(iterations=5)
+        self.store_process_image(
+            "./uploads/TableExtractor/5_vertical_dilated.jpg", self.v_dilated_image)
+        self.h_erosion_image(iterations=5)
+        self.store_process_image(
+            "./uploads/TableExtractor/6_horizontal_eroded.jpg", self.h_eroded_image)
+        self.h_dilation_image(iterations=5)
+        self.store_process_image(
+            "./uploads/TableExtractor/7_horizontal_dilated.jpg", self.h_dilated_image)
+        
         
